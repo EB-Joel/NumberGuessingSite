70f7be1699ca1a4249901fe684d06f326a9cfbbc
EB-Joel <105235872+EB-Joel@users.noreply.github.com>
2022-08-25 20:58:34 -0400

    created html, css, js
    will be adding react to complete this game

---
+++ script.js
@@ -0,0 +1,16 @@
+var announcement = document.querySelector("h5");
+var answer = Math.floor(Math.random() * 11);
+var guess = document.getElementById("number");
+var button = document.getElementById("guess");
+
+
+// console.log(guess.value)
+
+// const printResult = ()=>{
+//     announcement.innerText = guess.value;
+// }
+
+button.addEventListener(onclick, ()=>{
+    console.log('guess.innerText');
+
+});
