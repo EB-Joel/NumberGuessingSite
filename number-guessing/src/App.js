5736ddac9d835ce4a43520e8369dff0dd43087ba
Eric Berroa <ericberroa@live.com>
2022-08-29 16:31:47 -0400

    added Textboxjs

--- number-guessing/src/App.js
+++ number-guessing/src/App.js
@@ -1,1 +1,17 @@
-import react
+import React from 'react';
+import 'tachyons';
+import TextBox from './TextBox';
+
+
+class App extends React.Component {
+    render(){
+        return(
+        <div className='tc'>
+            <h1 className='f1'>Guess a number from 1-10</h1>
+            <TextBox />
+        </div>
+            );
+    }
+}
+
+export default App;
---
+++ number-guessing/src/NumberGame.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TextBox from "./TextBox";
+
+class NumberGame extends React.Component{
+    constructor(props){
+        super(props)
+        this.state
+    }
+
+}
