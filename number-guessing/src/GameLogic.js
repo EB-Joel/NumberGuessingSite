25a99dd2b23e375eee4275fa5eb7d49f9882940d
EB-Joel <105235872+EB-Joel@users.noreply.github.com>
2022-08-30 10:44:52 -0400

    Create GameLogic.js

---
+++ number-guessing/src/GameLogic.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const answer ={Math.floor(Math.random()*10)+1};
+
+
+
+class GameLogic extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={value:undefined}
+        this.guessSubmit = this.guessSubmit.bind(this)
+    }
+
+    while (answer > ({this.state.value})) {
+        if (condition) {
+            
+        }
+    }
+     
+    render(){
+        
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
